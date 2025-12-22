340d0840c61ff37f344b6da982a32460363a97ac
Daniel Simoes Guerreiro <dansimoe@c1r5s8.42lisboa.com>
2025-12-22 14:34:00 +0000

    updated

--- P01/ex1/ft_garden_data.py
+++ P01/ex1/ft_garden_data.py
@@ -1,16 +1,12 @@
 class Plant:
-    name: str
-    height: int
-
-    #def __init__(self, name: str, height: int, age: int) -> None:
-    #    self.name = name
-    #    self.height = height
-    #    self.age = age
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
 
     def print_info(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
-rose.name 
 
 rose: Plant = Plant("Rose", 25, 30)
 sunflower: Plant = Plant("Sunflower", 80, 45)
--- P01/ex4/ft_garden_security.py
+++ P01/ex4/ft_garden_security.py
@@ -1,13 +1,12 @@
 class SecurePlant:
-    def __init__(self, name: str, height: [int | None] = None, age: [int | None] = None) -> None:
+    def secure_create(self, height: int, age: int) -> int:
         if height < 0:
-            return print("Security: Negative height rejected")
+            return (print("Security: Negative height rejected"), 0)
         elif age < 0:
-            return print("Security: Negative age rejected")
-        self.age = age
-        self.name = name
-        self.growth = 0
-        print(f"Plant created: {self.name} ({self.height}cm, {self.age} days)")
+            return (print("Security: Negative age rejected"), 0)
+        else:
+            self.set_age(age)
+            self.set_height(height)
 
     def get_info(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
@@ -17,7 +16,18 @@ class SecurePlant:
         self.growth += growth
 
     def set_age(self, age: int = 1) -> None:
-        self.age += age
+        self.age = age
+
+    def set_height(self, age: int = 1) -> None:
+        self.age = age
+    
+    def __init__(self, name: str, height: [int | None] = None, age: [int | None] = None) -> None:
+        try:
+            secure_create(height, age)
+        self.name = name
+        self.age = age
+        self.growth = 0
+        print(f"Plant created: {self.name} ({self.height}cm, {self.age} days)")
 
 
 print("=== Garden Security System ===")
