c070ba3c03766ab73d00872433428c5800ede367
8Caxi8 <danielsimoesguerreiro@gmail.com>
2026-01-29 20:47:32 +0000

    P03 completed

--- P03/ex6/ft_analytics_dashboard.py
+++ P03/ex6/ft_analytics_dashboard.py
@@ -1,7 +1,7 @@
 def list_comprehensions(data: dict) -> None:
     high_scores: list[int] = []
     high_scorers: list[str] = []
-    scores_normalized: list[int] = []
+    active_players: list[int] = []
 
     high_scorers = (
         player
@@ -15,10 +15,11 @@ def list_comprehensions(data: dict) -> None:
         if value["total_score"] > 2000]
     print(f"High scores: {high_scores}")
 
-    scores_normalized = [
-        int(value["total_score"] / max(high_scores) * 100)
-        for value in data["players"].values()]
-    print(f"Normalized scores: {scores_normalized}\n")
+    active_players = [
+        info["player"]
+        for info in data["sessions"]
+        if info["duration_minutes"] > 100]
+    print(f"Last active players: {active_players}\n")
 
 
 def dict_comprehensions(data: dict) -> None:
@@ -62,13 +63,50 @@ def dict_comprehensions(data: dict) -> None:
             if info["total_score"] <= 1500
         })
     }
-    print(f"Score categories: {count_scores}")
+    print(f"Score categories: {count_scores}\n")
 
 
 def set_comprehensions(data: dict) -> None:
     players: set = set()
+    achievements: set = set()
+    favorite_mode: set = set()
 
-    players = 
+    players = {player for player in data["players"].keys()}
+    print(f"Unique players: {players}")
+
+    achievements = {achievement for achievement in data["achievements"]}
+    print(f"Unique achivements: {achievements}")
+
+    favorite_mode = {
+            type["favorite_mode"]
+            for type in data["players"].values()
+        }
+    print(f"Favorite modes: {favorite_mode}\n")
+
+
+def comb_analysis(data: dict) -> None:
+    no_players: int
+    high_scores: list
+    scores_normalized: list
+    top_performer: dict
+
+    no_players = len({player for player in data["players"].keys()})
+    print(f"Total Players: {no_players}")
+
+    high_scores = [
+        value["total_score"]
+        for value in data["players"].values()
+        if value["total_score"] > 2000]
+
+    scores_normalized = [
+        int(value["total_score"] / max(high_scores) * 100)
+        for value in data["players"].values()]
+    print(f"Normalized scores: {scores_normalized}")
+
+    top_performer = max(
+        data["players"],
+        key=lambda p: data["players"][p]["total_score"])
+    print(f"Top performer: {top_performer}\n")
 
 
 def analytics_dashboard(data: dict) -> None:
@@ -83,8 +121,10 @@ def analytics_dashboard(data: dict) -> None:
     print("=== Set Comprehension Examples ===")
     set_comprehensions(data)
 
+    print("=== Combined Analysis ===")
+    comb_analysis(data)
+
 
 if __name__ == "__main__":
-    data: dict = {'players': {'alice': {'level': 41, 'total_score': 2824, 'sessions_played': 13, 'favorite_mode': 'ranked', 'achievements_count': 5}, 'bob': {'level': 16, 'total_score': 4657, 'sessions_played': 27, 'favorite_mode': 'ranked', 'achievements_count': 2}, 'charlie': {'level': 44, 'total_score': 9935, 'sessions_played': 21, 'favorite_mode': 'ranked', 'achievements_count': 7}, 'diana': {'level': 3, 'total_score': 1488, 'sessions_played': 21, 'favorite_mode': 'casual', 'achievements_count': 4}, 'eve': {'level': 33, 'total_score': 1434, 'sessions_played': 81, 'favorite_mode': 'casual', 'achievements_count': 7}, 'frank': {'level': 15, 'total_score': 8359, 'sessions_played': 85, 'favorite_mode': 'competitive', 'achievements_count': 1}}, 'sessions': [{'player': 'bob', 'duration_minutes': 94, 'score': 1831, 'mode': 'competitive', 'completed': False}, {'player': 'bob', 'duration_minutes': 32, 'score': 1478, 'mode': 'casual', 'completed': True}, {'player': 'diana', 'duration_minutes': 17, 'score': 1570, 'mode': 'competitive', 'completed': False}, {'player': 'alice', 'duration_minutes': 98, 'score': 1981, 'mode': 'ranked', 'completed': True}, {'player': 'diana', 'duration_minutes': 15, 'score': 2361, 'mode': 'competitive', 'completed': False}, {'player': 'eve', 'duration_minutes': 29, 'score': 2985, 'mode': 'casual', 'completed': True}, {'player': 'frank', 'duration_minutes': 34, 'score': 1285, 'mode': 'casual', 'completed': True}, {'player': 'alice', 'duration_minutes': 53, 'score': 1238, 'mode': 'competitive', 'completed': False}, {'player': 'bob', 'duration_minutes': 52, 'score': 1555, 'mode': 'casual', 'completed': False}, {'player': 'frank', 'duration_minutes': 92, 'score': 2754, 'mode': 'casual', 'completed': True}, {'player': 'eve', 'duration_minutes': 98, 'score': 1102, 'mode': 'casual', 'completed': False}, {'player': 'diana', 'duration_minutes': 39, 'score': 2721, 'mode': 'ranked', 'completed': True}, {'player': 'frank', 'duration_minutes': 46, 'score': 329, 'mode': 'casual', 'completed': True}, {'player': 'charlie', 'duration_minutes': 56, 'score': 1196, 'mode': 'casual', 'completed': True}, {'player': 'eve', 'duration_minutes': 117, 'score': 1388, 'mode': 'casual', 'completed': False}, {'player': 'diana', 'duration_minutes': 118, 'score': 2733, 'mode': 'competitive', 'completed': True}, {'player': 'charlie', 'duration_minutes': 22, 'score': 1110, 'mode': 'ranked', 'completed': False}, {'player': 'frank', 'duration_minutes': 79, 'score': 1854, 'mode': 'ranked', 'completed': False}, {'player': 'charlie', 'duration_minutes': 33, 'score': 666, 'mode': 'ranked', 'completed': False}, {'player': 'alice', 'duration_minutes': 101, 'score': 292, 'mode': 'casual', 'completed': True}, {'player': 'frank', 'duration_minutes': 25, 'score': 2887, 'mode': 'competitive', 'completed': True}, {'player': 'diana', 'duration_minutes': 53, 'score': 2540, 'mode': 'competitive', 'completed': False}, {'player': 'eve', 'duration_minutes': 115, 'score': 147, 'mode': 'ranked', 'completed': True}, {'player': 'frank', 'duration_minutes': 118, 'score': 2299, 'mode': 'competitive', 'completed': False}, {'player': 'alice', 'duration_minutes': 42, 'score': 1880, 'mode': 'casual', 'completed': False}, {'player': 'alice', 'duration_minutes': 97, 'score': 1178, 'mode': 'ranked', 'completed': True}, {'player': 'eve', 'duration_minutes': 18, 'score': 2661, 'mode': 'competitive', 'completed': True}, {'player': 'bob', 'duration_minutes': 52, 'score': 761, 'mode': 'ranked', 'completed': True}, {'player': 'eve', 'duration_minutes': 46, 'score': 2101, 'mode': 'casual', 'completed': True}, {'player': 'charlie', 'duration_minutes': 117, 'score': 1359, 'mode': 'casual', 'completed': True}], 'game_modes': ['casual', 'competitive', 'ranked'], 'achievements': ['first_blood', 'level_master', 'speed_runner', 'treasure_seeker', 'boss_hunter', 'pixel_perfect', 'combo_king', 'explorer']}
-
+    data: dict = dict()
     analytics_dashboard(data)
