5e4800107993179a72581f2033adebd4a0c6ce2b
8Caxi8 <danielsimoesguerreiro@gmail.com>
2026-03-10 21:12:30 +0000

    P08 updated

--- P08/ex1/loading.py
+++ P08/ex1/loading.py
@@ -1,27 +1,228 @@
-import pandas
-import numpy
-import matplotlib
+import sys
+
+try:
+    import pandas as pd
+    import requests as rq
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import numpy as np
+except ImportError as e:
+    print(f"[ERROR]: {e.name} not installed.\n"
+          "Install with: pip install -r requirements.txt")
+    raise sys.exit(1)
 
 
-def check_pandas() -> None:
-    pass
+def check_dependencies() -> None:
+    print(f"[OK] pandas ({pd.__version__}) - Data manipulation ready")
+    print(f"[OK] requests ({rq.__version__}) - Network access ready")
+    print(f"[OK] matplotlib ({matplotlib.__version__}) - Visualization ready")
+    print(f"[OK] numpy ({np.__version__}) - Data generation ready\n")
 
 
-def check_requests() -> None:
-    pass
+def generate_data() -> pd.DataFrame:
+    no = list(np.random.rand(1000))
+
+    df = pd.DataFrame(no, columns=["matrix_data"])
+
+    return df
 
 
-def check_matplotlib() -> None:
-    pass
+def get_nasaapi_data(longitude: float, latitude: float,
+                     start: int, end: int) -> pd.DataFrame:
+    url = ("https://power.larc.nasa.gov/api/temporal/daily/point"
+           "?parameters=ALLSKY_SFC_SW_DWN"
+           "&community=RE"
+           f"&longitude={longitude}"
+           f"&latitude={latitude}"
+           f"&start={start}"
+           f"&end={end}"
+           "&format=JSON")
+
+    response = rq.get(url, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+
+    values = data["properties"]["parameter"]["ALLSKY_SFC_SW_DWN"]
+
+    df = pd.DataFrame(list(values.items()),
+                      columns=["date", "solar_radiation"])
+
+    df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")
+    df["solar_radiation"] = pd.to_numeric(df["solar_radiation"])
+
+    return df
+
+
+def analyze_data(df: pd.DataFrame, data_type: str
+                 ) -> tuple[pd.DataFrame, float, float, float]:
+    print("Analyzing Matrix data...")
+    print(f"Processing {len(df)} data points...")
+
+    mean = df[data_type].mean()
+    std = df[data_type].std()
+    median = df[data_type].median()
+
+    return df, mean, std, median
+
+
+def create_solar_visualization(statistics:
+                               tuple[pd.DataFrame, float, float, float],
+                               longitude: float,
+                               latitude: float
+                               ) -> None:
+    FILE_NAME = "solar_radiation_analysis.png"
+
+    df, mean, std, median = statistics
+
+    print("Generating visualization...")
+
+    start_date = df["date"].min().date()
+    end_date = df["date"].max().date()
+
+    plt.figure(figsize=(12, 6))
+
+    plt.scatter(
+        df["date"],
+        df["solar_radiation"],
+        color="orange",
+        s=30,
+        label="Daily Solar Radiation"
+    )
+
+    plt.plot(
+        df["date"],
+        df["solar_radiation"],
+        color="orange",
+        linewidth=0.5,
+        alpha=0.5,
+        )
+
+    plt.axhline(
+        mean,
+        color="red",
+        linestyle="--",
+        linewidth=2,
+        label=f"Mean ({mean:.2f})"
+    )
+
+    plt.axhline(
+        median,
+        color="blue",
+        linestyle="-.",
+        linewidth=2,
+        label=f"Median ({median:.2f})"
+    )
+
+    plt.axhline(
+        mean + std,
+        color="green",
+        linestyle=":",
+        linewidth=2,
+        label=f"Mean + Std ({(mean + std):.2f})"
+    )
+
+    plt.axhline(
+        mean - std,
+        color="green",
+        linestyle=":",
+        linewidth=2,
+        label=f"Mean - Std ({(mean - std):.2f})"
+    )
+
+    plt.xlabel("Date")
+    plt.ylabel("Solar Radiation (kWh/m²/day)")
+    plt.title("Daily Solar Radiation - NASA POWER Data\n"
+              f"{latitude:.4f}°, {longitude:.4f}° | "
+              f"{start_date} to {end_date}")
+
+    plt.grid(alpha=0.3)
+    plt.legend()
+
+    plt.xticks(rotation=45)
+
+    plt.tight_layout()
+    plt.savefig(FILE_NAME)
+
+    print("\nAnalysis complete!")
+    print(f"Results saved to: {FILE_NAME}")
+
+
+def create_matrix_visualization(statistics:
+                                tuple[pd.DataFrame, float, float, float]
+                                ) -> None:
+    FILE_NAME = "matrix_analysis.png"
+
+    df, mean, std, median = statistics
+
+    print("Generating visualization...")
+
+    plt.figure()
+    plt.title("Matrix Data Distribution")
+    plt.hist(
+        df["matrix_data"],
+        bins=30,
+        color="green",
+        alpha=0.6,
+        label="Data Distribution"
+    )
+    plt.axvline(
+        mean,
+        color="red",
+        linestyle="--",
+        linewidth=2,
+        label=f"Mean ({mean:.2f})"
+    )
+    plt.axvline(
+        median,
+        color="blue",
+        linestyle="-.",
+        linewidth=2,
+        label=f"Median ({median:.2f})"
+    )
+    plt.axvline(
+        mean - std,
+        color="orange",
+        linestyle=":",
+        linewidth=2,
+        label="Mean - Std"
+    )
+    plt.axvline(
+        mean + std,
+        color="orange",
+        linestyle=":",
+        linewidth=2,
+        label="Mean + Std"
+    )
+    plt.savefig(FILE_NAME)
+    print("\nAnalysis complete!")
+    print(f"Results saved to: {FILE_NAME}")
 
 
 def main() -> None:
     print("\nLOADING STATUS: Loading programs...\n")
-
     print("Check dependencies:")
-    check_pandas()
-    check_requests()
-    check_matplotlib()
+    longitude = -9.1393
+    latitude = 38.7223
+
+    check_dependencies()
+    try:
+        data = get_nasaapi_data(longitude, latitude, 20250101, 20260101)
+        data_type = "solar_radiation"
+
+    except (rq.exceptions.HTTPError, rq.exceptions.ConnectionError,
+            rq.exceptions.Timeout) as e:
+        print(f"Requests Error: {e}")
+        print("Generating backup data...\n")
+        data = generate_data()
+        data_type = "matrix_data"
+
+    statistics = analyze_data(data, data_type)
+
+    if data_type == "solar_radiation":
+        create_solar_visualization(statistics, longitude, latitude)
+    else:
+        create_matrix_visualization(statistics)
 
 
 if __name__ == "__main__":
---
+++ P08/ex2/oracle.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+
+def load_configuration() -> dict:
+    load_dotenv()
+
+    config = {
+        "MATRIX_MODE": os.getenv("MATRIX_MODE"),
+        "DATABASE_URL": os.getenv("DATABASE_URL"),
+        "API_KEY": os.getenv("API_KEY"),
+        "LOG_LEVEL": os.getenv("LOG_LEVEL"),
+        "ZION_ENDPOINT": os.getenv("ZION_ENDPOINT"),
+    }
+
+    return config
+
+
+def validate_configuration(config: dict) -> bool:
+    missing = [key for key, value in config.items() if not value]
+
+    if missing:
+        print("[WARNING]: Missing configuration variables:")
+        for key in missing:
+            print(f" - {key}")
+            print("\nPlease configure your .env "
+                  "file or set environment variables.")
+
+            return False
+
+    return True
+
+
+def simulate_environment_behavior(config: dict) -> None:
+    mode = config["MATRIX_MODE"]
+
+    print("\nConfiguration loaded:")
+    print(f"Mode: {mode}")
+
+    if mode == "development":
+        print("Database: Connected to local instance")
+    elif mode == "production":
+        print("Database: Connected to secure production cluster")
+    else:
+        print("[WARNING]: Unknown MATRIX_MODE")
+
+    if config["API_KEY"] == "secret123":
+        print("API Acess: Authenticated")
+    elif config["API_KEY"] == "dansimoe123":
+        print("API Acess: Top-Level Access")
+    else:
+        print("API ACCESS: Key not valid!")
+
+    print(f"Log Level: {config['LOG_LEVEL']}")
+    print("Zion Network: Online")
+
+
+def security_check() -> None:
+    print("\nEnvironment security check:")
+
+    print("[OK] No hardcoded secrets detected")
+    if os.path.exists(".env"):
+        print("[OK] .env file properly configured")
+    else:
+        print("[WARNING]: No .env file detected")
+
+    print("[OK] Production overrides available")
+
+
+def main() -> None:
+    print("\nORACLE STATUS: Reading the Matrix...")
+
+    config: dict = load_configuration()
+
+    if not validate_configuration(config):
+        sys.exit(1)
+
+    simulate_environment_behavior(config)
+    security_check()
+
+    print("\nThe Oracle sees all configurations.")
+
+
+if __name__ == "__main__":
+    main()
